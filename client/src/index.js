dc71a8529068cae7d08d1aa9ec165e8a21c7bad4
Greg Brisebois <gregoryjjb@gmail.com>
2018-09-11 16:38:14 -0700

    Removed frontend

--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import AppContainer from './containers/AppContainer'
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<AppContainer />, document.getElementById('root'));
-registerServiceWorker();
