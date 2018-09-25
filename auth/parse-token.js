a17745156a869ad21eee4587af16e88c2fc953e4
Greg Brisebois <gregoryjjb@gmail.com>
2018-09-25 15:38:11 -0700

    Google token parsing function

---
+++ auth/parse-token.js
@@ -0,0 +1,35 @@
+const { OAuth2Client } = require("google-auth-library");
+
+const audience = process.env.GOOGLE_CLIENT_ID;
+
+const gapiClient = new OAuth2Client(audience, "", "");
+
+if (audience) {
+    console.log("Google Sign-in Initialized");
+    console.log("\tClient ID:", audience);
+} else {
+    console.log(
+        "ERROR: No Google Client ID set, sign-in WILL FAIL. Set env variable GOOGLE_CLIENT_ID"
+    );
+}
+
+/**
+ * Checks a google token for validity
+ * @param {*} idToken The google token
+ * @param {*} onSuccess Success callback
+ * @param {*} onFailure Failure callback
+ */
+const parser = (idToken, onSuccess, onFailure) => {
+    if (audience) {
+        gapiClient
+            .verifyIdToken({ idToken, audience })
+            .then(login => {
+                onSuccess(login.getPayload());
+            })
+            .catch(error => {
+                if (onFailure) onFailure(error);
+            });
+    } else {
+        onFailure({ message: "No audience specified" });
+    }
+};
