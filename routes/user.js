e7a06c8c6889fdf84e0eda07f12bd14028ac3135
Greg Brisebois <gregoryjjb@gmail.com>
2018-11-11 15:32:01 -0800

    Plan calculations

--- routes/user.js
+++ routes/user.js
@@ -33,6 +33,8 @@ router.put("/plan", async (req, res) => {
 
     const user = await models.User.findOne({ where: { id: userId } });
 
+    let round = Math.round;
+
     if (user) {
         const currentNum = Number(currentWeekly);
         const goalNum = Number(goalDistance);
@@ -45,16 +47,39 @@ router.put("/plan", async (req, res) => {
             const cmd = `${py} predict.py ${goalNum}`;
             const { stdout, stderr } = await exec(cmd, { cwd: "./ml" });
 
-            const targetWeekly = Math.round(Number(stdout));
+            const targetWeekly = round(Number(stdout));
             const increment = (targetWeekly - currentNum) / weeksNum;
 
             let weeklyPlan = [];
 
             for (let i = 1; i <= weeksNum; i++) {
-                weeklyPlan.push(Math.round(currentNum + increment * i));
+                weeklyPlan.push(round(currentNum + increment * i));
             }
 
-            res.json({ weeklyTarget: targetWeekly, weeklyPlan });
+            round = x => Number((Math.round(x * 2) / 2).toFixed(1));
+
+            let dailyPlan = weeklyPlan.map(miles => {
+                if (miles < 30) {
+                    let long = round((miles * 0.2) / 1); // 20% Long x1
+                    let same = round((miles * 0.7) / 3); // 70% Same x3
+                    let short = round((miles * 0.1) / 1); // 10% Short x1
+
+                    return [same, 0, same, 0, same, 0, short];
+                } else if (miles >= 30 && miles < 60) {
+                    let long = round((miles * 0.2) / 1); // 20% Long x1
+                    let same = round((miles * 0.5) / 3); // 50% Same x3
+                    let short = round((miles * 0.3) / 2); // 30% Short x2
+
+                    return [long, same, short, same, short, same, 0];
+                } else {
+                    let long = round((miles * 0.2) / 1); // 20% Long x1
+                    let same = round((miles * 0.6) / 4); // 60% Same x4
+                    let short = round((miles * 0.2) / 2); // 20% Short x2
+                    return [long, short, same, same, short, same, same];
+                }
+            });
+
+            res.json({ weeklyTarget: targetWeekly, weeklyPlan, dailyPlan });
         }
     } else {
         res.status(401).end("No user logged in");
